6c5c2406fd65b46641b7fa5185f48cd15178ab66
João Duduch <jpduduch@gmail.com>
2026-06-18 16:18:51 +0200

    wip: writing tests (user model)

--- network/models.py
+++ network/models.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class User(AbstractUser):
     # o parâmetro symmetrical significa que A -> B mas B !<- A
-    following = models.ManyToManyField('self', related_name='followers', blank=True, symmetrical=False) 
+    following = models.ManyToManyField('self', related_name='followers', blank=True, symmetrical=False)
+
+    def clean(self):
+        if self.following == self:
+            raise ValidationError("User cannot follow self.")
 
 
 class Post(models.Model):
--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+
+# B. Posts
+
+# Se usuário consegue postar.
+# Se usuário consegue editar somente os próprios posts.
+# Se usuário consegue apagar somente os próprios posts.
+# Se usuário consegue dar like em posts (deles próprios e de terceiros) somente se o post não tiver um like deste usuário
+# Se usuário consegue remover o like se houver um like deste mesmo usuário.
+
+class ModelFollowTests(TestCase):
+    def setUp(self):
+        self.user_a = User.objects.create_user(username='user_a', password='123')
+        self.user_b = User.objects.create_user(username='user_b', password='123')
+
+    def test_user_can_follow_another(self):
+        self.user_a.following.add(self.user_b)
+        user_follows = self.user_a.following.filter(pk=self.user_b.id).exists()
+        self.assertTrue(user_follows)
+
+    def test_user_does_not_follow_each_other_automatically(self):
+        self.user_a.following.add(self.user_b)
+        user_a_follows = self.user_a.following.filter(pk=self.user_b.id).exists()
+        self.assertTrue(user_a_follows)
+
+        user_b_follows_user_a = self.user_b.following.filter(pk=self.user_a.id).exists()
+        self.assertFalse(user_b_follows_user_a)
+
+    def test_user_can_unfollow(self):
+        self.user_a.following.add(self.user_b)
+        user_follows = self.user_a.following.filter(pk=self.user_b.id).exists()
+        self.assertTrue(user_follows)
+
+        self.user_a.following.remove(self.user_b)
+        user_follows = self.user_a.following.filter(pk=self.user_b.id).exists()
+        self.assertFalse(user_follows)
