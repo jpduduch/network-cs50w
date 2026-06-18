02734cbed1ac6e29c9f89eeba813cc84cfc1eef4
João Duduch <jpduduch@gmail.com>
2026-06-18 12:48:38 +0200

    fix: Adjust models to simplify tables

--- network/models.py
+++ network/models.py
@@ -3,18 +3,12 @@ from django.db import models
 
 
 class User(AbstractUser):
-    following = models.ManyToManyField('User', blank=True)
+    # o parâmetro symmetrical significa que A -> B mas B !<- A
+    following = models.ManyToManyField('self', related_name='followers', blank=True, symmetrical=False) 
 
 
 class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
-
-
-class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
-    liker = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    date = models.DateTimeField(auto_now_add=True)
-    like = models.BooleanField()
-
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
