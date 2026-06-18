6deb39f094e13bf79f5b8eb1a294847e4bee87d2
João Duduch <jpduduch@gmail.com>
2026-06-18 10:52:29 +0200

    feat: bring models.py from create-models branch

--- network/models.py
+++ network/models.py
@@ -3,4 +3,18 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField('User', blank=True)
+
+
+class Post(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    liker = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    like = models.BooleanField()
+
