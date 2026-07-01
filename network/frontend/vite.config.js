37ee37c67c9c828478af344b11c69c6c293ea8ef
João Duduch <jpduduch@gmail.com>
2026-07-01 15:56:29 +0200

    setup: react test environment

--- network/frontend/vite.config.js
+++ network/frontend/vite.config.js
@@ -16,5 +16,10 @@ export default defineConfig({
       }
     }
   },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setup.js',
+  },
   base: '/static/network/react/',
 })
