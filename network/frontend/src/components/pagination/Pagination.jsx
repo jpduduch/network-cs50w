83bf0b2bb27142c17b98ec33ec20a42c7a8b08fa
João Duduch <jpduduch@gmail.com>
2026-07-24 18:41:21 +0200

    wip: implement front-end logic of pagination

--- network/frontend/src/components/pagination/Pagination.jsx
+++ network/frontend/src/components/pagination/Pagination.jsx
@@ -4,6 +4,14 @@ import PageItem from './PageItem';
 function Pagination({ page, onSelect }) {
     const { current: currentPage, range: pageRange, has_prev: hasPrev, has_next: hasNext } = page;
 
+    const pagesArray = [];
+    const max = Math.min(currentPage + 4, pageRange);
+    const min = Math.max(max - 4, 1);
+
+    for (let i = min; i <= max; i++) {
+        pagesArray.push(i);
+    }
+
     return (
         <ul className="pagination">
             {hasPrev ? (
